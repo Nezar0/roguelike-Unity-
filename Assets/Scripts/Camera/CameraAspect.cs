bc9783ae86029c970bad0067f468f65ecb52b49c
Nezar0 <8952905o213@gmail.com>
2021-03-16 18:09:48 +0700

    Aspect Camera

---
+++ Assets/Scripts/Camera/CameraAspect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAspect : MonoBehaviour
+{
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.aspect = 2960/1440;
+    }
+}
