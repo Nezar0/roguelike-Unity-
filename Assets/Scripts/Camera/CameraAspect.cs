e8e9eed9b3c03017d991925da2248f068599a306
Nezar0 <8952905o213@gmail.com>
2021-03-24 21:06:49 +0700

    анимация и враг

--- Assets/Scripts/Camera/CameraAspect.cs
+++ Assets/Scripts/Camera/CameraAspect.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraAspect : MonoBehaviour
+namespace CameraModule
 {
-    Camera cam;
-
-    void Start()
+    public class CameraAspect : MonoBehaviour
     {
-        cam = GetComponent<Camera>();
-        cam.aspect = 2960/1440;
+        Camera cam;
+
+        void Start()
+        {
+            cam = GetComponent<Camera>();
+            cam.aspect = 2960 / 1440;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,13 +1,27 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
+    public Joystick joystick;
+    public ControlType controlType;
+    public enum ControlType { PC, Android}
+
     public float moveSpead;
 
-    public Rigidbody2D rb;
-
+    private Rigidbody2D rb;
     private Vector2 moveDir;
+    private Animator anim;
+    private bool facingLeft = false;
 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
     private void Update()
     {
         ProcessInputs();   
@@ -19,14 +33,45 @@ public class PlayerMovement : MonoBehaviour
 
     void ProcessInputs()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        if (controlType == ControlType.PC)
+        {
+            float moveX = Input.GetAxisRaw("Horizontal");
+            float moveY = Input.GetAxisRaw("Vertical");
+            moveDir = new Vector2(moveX, moveY).normalized;
+        }
+        else if (controlType == ControlType.Android)
+        {
+            float moveX = joystick.Horizontal;
+            float moveY = joystick.Vertical;
+            moveDir = new Vector2(moveX, moveY).normalized;
+        }
+        if (moveDir.x == 0 || moveDir.y == 0) 
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
 
-        moveDir = new Vector2(moveX, moveY).normalized;
+        if (facingLeft && moveDir.x > 0)
+        {
+            Flip();
+        }
+        else if (!facingLeft && moveDir.x < 0) 
+        {
+            Flip();
+        }
     }
 
     void Move()
     {
         rb.velocity = new Vector2(moveDir.x * moveSpead, moveDir.y * moveSpead);
+    }
+
+    private void Flip()
+    {
+        facingLeft = !facingLeft;
+        GetComponent<SpriteRenderer>().flipX = facingLeft;
     }
 }
